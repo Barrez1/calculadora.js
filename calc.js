8452e61860d66aec0266a4d99fb6606d6563222a
Barrez1 <barresepedro18@gmail.com>
2023-05-31 14:41:34 -0300

    calculadora

---
+++ calc.js
@@ -0,0 +1,10 @@
+var SomaFunc = require("../soma");
+var SubFunc = require("../sub");
+var MultiFunc=require("../multi");
+var DivFunc=require("../div");
+
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultiFunc(5,2));
+console.log(DivFunc(15,2));
